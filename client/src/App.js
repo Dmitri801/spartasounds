60cabc74e8752a6688afa9ce90a420c3768e8fbd
Dmitriy Sharshiner <dmi@Dmitriys-Mac-mini.local>
2018-08-31 09:47:23 -0600

    running servers concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Navbar from './components/Layout/Navbar';
 import Home from './components/Home';
 class App extends Component {
+  componentDidMount() {
+    axios.get('/api/products/genres').then(response => console.log(response.data))
+  }
   render() {
     return (
       <div style={{height: '2000px'}} className="App">
