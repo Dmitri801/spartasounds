800dc788a20b02b9e4529c0e413b515cdb4120dd
Dmitri <dsharsh@hotmail.com>
2018-10-04 13:06:07 -0600

    troubleshooting heroku

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.use("/", genres);
 // DEFAULT
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
